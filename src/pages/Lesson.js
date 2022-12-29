5eba0500e515c74d66340a1b948a89ff3ae6c7a4
Micheal Limones <micheal@zestylimones.com>
2022-12-28 20:34:32 -0600

    lesson information now populates on lesson's page.

--- src/pages/Lesson.js
+++ src/pages/Lesson.js
@@ -1,1 +1,21 @@
 import { useParams } from 'react-router-dom';
+import lessons from '../lessons';
+import { Link } from 'react-router-dom';
+
+function Lesson() {
+  const { lessonId } = useParams();
+  const lesson = lessons.find((lesson) => lesson.id === parseInt(lessonId));
+  return (
+    <div className="Lesson page">
+      <header>
+        <p>
+          <Link to={'/'}> Back to Home</Link>
+        </p>
+        <h1>{lesson.title}</h1>
+        <p>{lesson.description}</p>
+      </header>
+    </div>
+  );
+}
+
+export default Lesson;
