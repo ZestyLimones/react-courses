34d0379fb3a674495f85fcf63624af7ac76dc431
Micheal Limones <micheal@zestylimones.com>
2022-12-28 19:44:53 -0600

    added home page and lessons.

---
+++ src/lessons.js
@@ -0,0 +1,32 @@
+const lessons = [
+  {
+    id: 1,
+    title: 'Medical Stuff',
+    description:
+      'this is some information about the first lesson. I need to figure out how to add a a powerpoint',
+    vimeoId: 7842645,
+  },
+  {
+    id: 2,
+    title: 'Medical Stuff part 2',
+    description:
+      'this is some information stuff. I need to figure out how to add a a powerpoint',
+    vimeoId: 54646482,
+  },
+  {
+    id: 3,
+    title: 'Medical Stuf part 3f',
+    description:
+      'this is some information about stuff. I need to figure out how to add a a powerpoint',
+    vimeoId: 549816,
+  },
+  {
+    id: 4,
+    title: 'Medical Stuff part 4',
+    description:
+      'this is some information about stuff. I need to figure out how to add a a powerpoint',
+    vimeoId: 465318,
+  },
+];
+
+export default lessons;
---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import lessons from '../lessons';
+import LessonSummary from '../components/LessonSummary';
+
+function Home() {
+  return (
+    <div className="Home Page">
+      <header>
+        <h1>Orlando's Medical Teachings</h1>
+      </header>
+      {lessons.map((lesson) => {
+        <LessonSummary lesson={lesson} key={lesson.id} />;
+      })}
+    </div>
+  );
+}
+
+export default Home;
